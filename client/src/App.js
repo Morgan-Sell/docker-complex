1c6410ee9c30b1cea94f4087dd4cdf95ae917dc8
Morgan-Sell <morganpsell@gmail.com>
2023-12-23 15:55:28 -0500

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Multi-container Docker!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
