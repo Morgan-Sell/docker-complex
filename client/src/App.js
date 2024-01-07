733097deef600ab5f8fa60f16e517e7189fafa2a
Morgan-Sell <morganpsell@gmail.com>
2024-01-07 17:37:07 -0500

    change client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Multi-container Docker!
+            Let's gooooo Docker!!!!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
