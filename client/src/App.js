1e137ee1ad5d69b71f17b384da9557e027627091
Morgan-Sell <morganpsell@gmail.com>
2024-01-09 10:58:09 -0500

    update client/src/app.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Let's gooooo Docker!!!!
+            Pleaseeeee work!!!!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
