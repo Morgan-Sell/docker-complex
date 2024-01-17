ce4d4eff04b44ad0c659a0aa2d95489185dc6827
Morgan-Sell <morganpsell@gmail.com>
2024-01-17 17:30:03 -0500

    change app.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Leave the port!!!
+            Fib Caclulator Deployed!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
